68ecdf2036ada126d0472e9b5b45bab5102f1daa
Claudia Maganhi <claudia.maganhi@gmail.com>
2019-12-17 12:20:18 -0300

    round views

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var collectionView: [UIView]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        roundViews()
+        
+    }
+    
+    func roundViews() {
+        for view in collectionView {
+            view.layer.cornerRadius = view.frame.width / 2
+        }
     }
 
 
