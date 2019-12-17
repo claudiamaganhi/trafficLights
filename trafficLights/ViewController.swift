82c0b2806271ad7c684b9d7c297cd7e02b5bb1fe
Claudia Maganhi <claudia.maganhi@gmail.com>
2019-12-17 12:07:41 -0300

    Initial Commit

---
+++ trafficLights/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trafficLights
+//
+//  Created by Claudia Maganhi on 17/12/19.
+//  Copyright © 2019 Claudia Maganhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
